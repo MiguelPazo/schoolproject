4e586e74896192af749fc0d4cd037baf49c17294
Miguel Rodrigo Pazo Sanchez <miguel.ps19@gmail.com>
2012-07-17 23:22:04 -0500

    interfaz de pizarra agregada y plugin de dock icons

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -8,28 +8,4 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $autoLoader->registerNamespace("App_");
         $autoLoader->registerNamespace("Models_");
     }
-
-    public function _initView()
-    {
-        $this->bootstrap('layout');
-        $layout = $this->getResource('layout');
-        $view = $layout->getView();
-        
-        $view->doctype(Zend_View_Helper_Doctype::HTML5);
-        $view->headTitle('School Project')->getSeparator(' | ');
-        
-        $view->headMeta()->appendName('description','School Project');
-        $view->headMeta()->appendName('author','Team school project');
-        $view->headMeta()->appendName('viewport','width=device-width');
-        
-        $view->headLink()->appendStylesheet('/styles/style.css');
-        $view->headLink()->appendStylesheet('/styles/main.css');
-        
-        $view->headScript()->appendFile('/js/libs/jquery-1.7.1.js');
-        $view->headScript()->appendFile('/js/plugins.js');
-        $view->headScript()->appendFile('/js/googleAnalytics.js');
-        $view->headScript()->appendFile('/js/main.js');
-        $view->headScript()->appendFile('/js/libs/modernizr-2.5.2.min.js');
-        
-    }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    
+    $('#dock2').Fisheye(
+    {
+        maxWidth: 40,
+        items: 'a',
+        itemsText: 'span',
+        container: '.dock-container2',
+        itemWidth: 40,
+        proximity: 80,
+        alignment : 'left',
+        valign: 'bottom',
+        halign : 'center'
+    }
+    )
 })
