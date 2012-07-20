099bc65c69f41b894e5356d3e5efe9a484afc79d
Juan Castro <jn90pp@gmail.com>
2012-07-20 14:18:04 -0500

    abstraccion tabla alumno

--- application/models/alumno.php
+++ application/models/alumno.php
@@ -2,8 +2,58 @@
 
 class Models_Alumno extends Application_Model_AlumnoTable
 {
-    public function update() {
-        
+    protected $_dbTable;
+    
+    public function getDbTable()
+    {
+        if(null===$this->_dbTable){
+            $this->_dbTable='Application_Model_AlumnoTable';
+        }
+        return $this->_dbTable;
+    }
+
+    public function listar($cod_alumno,Application_Model_AlumnoTable $oalumno)
+    {
+        $resultado=  $this->getDbTable()->find($cod_alumno);
+        if(0==count($resultado)){
+            return;
+        }
+        $fila=$resultado->current();
+        $oalumno->setCod_alumno($fila->cod_alumno)
+                ->setDatos_Personales_cod_personal($fila->Datos_Personales_cod_personal)
+                ->setUsuario_cod_usuario($fila->Usuario_cod_usuario)
+                ->setCorreo($fila->correo)
+                ->setEstado($fila->estado);        
+    }
+
+    public function listarTodo(){
+        $resultado=$this->getDbTable()->fetchAll();
+        $oalumnos=array();
+        foreach ($resultado as $fila) {
+            $nalumno=new Application_Model_AlumnoTable();
+            $nalumno->setCod_alumno($fila->cod_alumno)
+                    ->setDatos_Personales_cod_personal($fila->Datos_Personales_cod_personal)
+                    ->setUsuario_cod_usuario($fila->Usuario_cod_usuario)
+                    ->setCorreo($fila->correo)
+                    ->setEstado($fila->estado);
+            $oalumnos[]=$nalumno;
+        }
+        return $oalumnos;
     }
     
+    public function guardar(Application_Model_AlumnoTable $oalumno){
+        $data=array(
+            'Datos_Personales_cod_personal'=>$oalumno->getDatos_Personales_cod_personal(),
+            'Usuario_cod_usuario'=>$oalumno->getUsuario_cod_usuario(),
+            'correo'=>$oalumno->getCorreo(),
+            'estado'=>$oalumno->getEstado(),
+        );
+        
+        if(null===($cod_alumno=$oalumno->cod_alumno)){
+            unset($data['cod_alumno']);
+            $this->getDbTable()->insert($data);
+        }else{
+            $this->getDbTable()->update($data,array('cod_alumno=?'=>$cod_alumno));
+        }
+    }
 }
--- application/models/table/alumnoTable.php
+++ application/models/table/alumnoTable.php
@@ -4,6 +4,73 @@ class Application_Model_AlumnoTable extends Zend_Db_Table_Abstract
 {
     protected $_name = 'Alumno';
     
-    protected $idAlumno;
+    protected $_cod_alumno;
+    protected $_Datos_Personales_cod_personal;
+    protected $_Usuario_cod_usuario;
+    protected $_correo;
+    protected $_estado;
     
+    public function __construct() {
+        $this->_cod_alumno=0;
+        $this->_Datos_Personales_cod_personal=0;
+        $this->_Usuario_cod_usuario=0;
+        $this->_correo="";
+        $this->_estado="";
+    }
+    
+    public function setDatos_Personales_cod_personal($cod_personal)
+    {
+        $this->_Datos_Personales_cod_personal=(int) $cod_personal;
+        return $this;
+    }
+    
+    public function getDatos_Personales_cod_personal()
+    {
+        return $this->_Datos_Personales_cod_personal;
+    }
+    
+    public function setUsuario_cod_usuario($cod_usuario)
+    {
+        $this->_Usuario_cod_usuario=(int) $cod_usuario;
+        return $this;        
+    }
+    
+    public function getUsuario_cod_usuario()
+    {
+        return $this->_Usuario_cod_usuario;
+    }
+    
+    public function setCorreo($correo)
+    {
+        $this->_correo=(string)$correo;
+        return $this;
+    }
+    
+    public function getCorreo()
+    {
+        return $this->_correo;
+    }
+    
+    public function setEstado($estado)
+    {
+        $this->_estado=(string)$estado;
+        return $this;
+    }
+    
+    public function getEstado()
+    {
+        return $this->_estado;
+    }
+    
+    public function setCod_alumno($cod_alumno)
+    {
+        $this->_cod_alumno=(int)$cod_alumno;
+        return $this;
+    }
+    
+    public function getCod_alumno()
+    {
+        return $this->_cod_alumno;
+    }
+
 }
