c155d1a220828d4f9b3f065c8d7ab0362bc13aad
Miguel Rodrigo Pazo Sanchez <miguel.pazo>
2012-07-26 19:32:05 -0500

    Correcciones

--- application/models/AlumnoProccess.php
+++ application/models/AlumnoProccess.php
@@ -2,15 +2,58 @@
 
 class Application_Model_AlumnoProccess extends Application_Model_Alumno
 {
-    public function getAll(){
-        $allData = $this->fetchAll();
-        /*$allDataObject debe contener todos el array de $allData, pero cada fila como objeto Alumno*/
-        $allDataObject = array() ;
+    protected $_dbTable;
+    
+    public function getDbTable()
+    {
+        if(null===$this->_dbTable){
+            $this->_dbTable='Application_Model_AlumnoTable';
+        }
+        return $this->_dbTable;
+    }
+
+    public function listar($cod_alumno,Application_Model_AlumnoTable $oalumno)
+    {
+        $resultado=  $this->getDbTable()->find($cod_alumno);
+        if(0==count($resultado)){
+            return;
+        }
+        $fila=$resultado->current();
+        $oalumno->setCod_alumno($fila->cod_alumno)
+                ->setDatos_Personales_cod_personal($fila->Datos_Personales_cod_personal)
+                ->setUsuario_cod_usuario($fila->Usuario_cod_usuario)
+                ->setCorreo($fila->correo)
+                ->setEstado($fila->estado);        
+    }
+
+    public function listarTodo(){
+        $resultado=$this->getDbTable()->fetchAll();
+        $oalumnos=array();
+        foreach ($resultado as $fila) {
+            $nalumno=new Application_Model_AlumnoTable();
+            $nalumno->setCod_alumno($fila->cod_alumno)
+                    ->setDatos_Personales_cod_personal($fila->Datos_Personales_cod_personal)
+                    ->setUsuario_cod_usuario($fila->Usuario_cod_usuario)
+                    ->setCorreo($fila->correo)
+                    ->setEstado($fila->estado);
+            $oalumnos[]=$nalumno;
+        }
+        return $oalumnos;
     }
     
-    public function getOne($id){
-        $oneData = $this->fetchRow();
-        /*$oneDataObject debe contener el registro de $oneData pero como un objeto Alumno*/
-        $oneDataObject = array() ;
+    public function guardar(Application_Model_AlumnoTable $oalumno){
+        $data=array(
+            'Datos_Personales_cod_personal'=>$oalumno->getDatos_Personales_cod_personal(),
+            'Usuario_cod_usuario'=>$oalumno->getUsuario_cod_usuario(),
+            'correo'=>$oalumno->getCorreo(),
+            'estado'=>$oalumno->getEstado(),
+        );
+        
+        if(null===($cod_alumno=$oalumno->cod_alumno)){
+            unset($data['cod_alumno']);
+            $this->getDbTable()->insert($data);
+        }else{
+            $this->getDbTable()->update($data,array('cod_alumno=?'=>$cod_alumno));
+        }
     }
 }
